31c328b8ae68e615eb460d1d8a534528727c71da
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-24 12:06:56 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,24 @@
 class Artist
   attr_accessor :name
 
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+
+  end
+  
+  def add_song
+
+  end
+
+  def add_song_by_name
+
+  end
+
+  def self.song_count
+
+  end
+
 end
