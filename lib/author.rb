a8aac43f5ee765636ffee25b146cc6fc420b8dd2
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-24 12:25:14 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,12 +9,13 @@ class Author
 
   end
 
-  def add_post
-
+  def add_post(post)
+    post.author = self
   end
 
-  def add_post_by_title
-
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.add_post(post)
   end
 
   def self.post_count
