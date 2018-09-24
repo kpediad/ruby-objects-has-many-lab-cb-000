d521c183cd73512b290f17b147dfd9703029138b
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-24 12:12:02 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,4 +2,15 @@ class Post
   @@all = []
   attr_accessor :title, :author
 
+  def initialize(title)
+    @title = title
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    self.author.name
+  end
 end
