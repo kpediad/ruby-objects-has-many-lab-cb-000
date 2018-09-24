73f62e40ae26fcffad635e549b77503522c163f8
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-24 12:13:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,4 +2,18 @@ class Song
   @@all = []
   attr_accessor :name, :artist
 
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    self.artist.name
+  end
+    
+  end
+
 end
